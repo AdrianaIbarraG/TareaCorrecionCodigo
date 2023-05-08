48bcab12026e27a373b83e7e875f54f6899f3aa6
AGIG <adriana.ibarra.lae@gmail.com>
2023-05-08 00:34:23 -0600

    Se modifica código para que cumpla con su función

--- formulario.js
+++ formulario.js
@@ -1,97 +1,79 @@
-var formulario = document.querySelector("#form")
+/* El código permite registrar invitados para una fiesta, una vez que se ingresan los datos, 
+en la parte "Lista de invitados" se mostrarán los asistente y estos contarán con un botón de "eliminar invitado"
+para remover a los asistentes que se requieran*/
 
-formulario.onsubmit = function(e) {
+let formulario = document.querySelector(".formulario");
+//Se trae la clase del formulario
+//Se cambian var por let
 
-  e.prevent();
+formulario.onsubmit = function(event) {
+  event.preventDefault(); 
+  //Se corrije e por event y se corrije el preventDefault
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  //Se cambian nombre de variables para trabajar más fácil con ellas 
+  let txtNombre = formulario.elements.names;
+  let txtEdad = formulario.elements.age;
+  let txtNacionalidad = formulario.elements.nationality;
+  //Se borran brakets con el número del elemento del arreglo y se pone el nombre de los id
 
-  var nombre = n.value
-  var edad = e.value
+  let nombre = txtNombre.value;
+  let edad = parseInt(txtEdad.value); // Se convierte edad a número
+  let nacionalidad = txtNacionalidad.value;
+  //Se eliminan los pasos extra de nacionalidad 
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  console.log(nombre, edad);
+  console.log(nacionalidad);
 
   if (nombre.length === 0) {
-    n.classList.add("error")
-  }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
+    txtNombre.classList.add("error");
+    alert("Ingresa tu nombre")
+  }else{
+    txtNombre.classList.remove("error");
+  } // if nombre === 0
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
-}
+  if (edad < 18 || edad > 120 || isNaN(edad)) {//se agrega evaluación para que no se deje en blanco la edad
+    txtEdad.classList.add("error");
+    alert ("La edad debe ser en número, mayor a 18 y menor a 120")
+  }else {
+    txtEdad.classList.remove("error");
+  }// if edad 
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
+  if (txtNombre.classList.value === "" && txtEdad.classList.value === "") {
+    agregarInvitado(nombre, edad, nacionalidad);
+    names.value="";
+    age.value="";
+    nationality.value=""; 
+  }// if de campos; se cambiaron signos del if para que cumpla con los pasos establecidos y se agrega claassList
+}//formulario onsubmit
+
 
 function agregarInvitado(nombre, edad, nacionalidad) {
 
-  if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
+  let lista = document.getElementById("lista-de-invitados");
+  let elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  //Se corrige added por add
+  lista.appendChild(elementoLista);
+
+  // Se elimina código repetido
+  function crearElemento(descripcion, valor) {
+  let span = document.createElement("span");
+  span.textContent = descripcion + ": " + valor; //Se agrega valor para que muestre datos
+  elementoLista.appendChild(span);
+  elementoLista.appendChild(document.createElement("br"));
   }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
-  }
 
-var lista = document.getElementById("lista-de-invitados")
-
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
-
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
-
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
 
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
+  let botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  elementoLista.appendChild(botonBorrar);
 
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
-  }
-}
+  botonBorrar.onclick = function() {
+  // this.parentNode.style.display = 'none';
+  botonBorrar.parentNode.remove();
+    }//btn Borrar
+}//agregar invitado
